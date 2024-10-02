a5545defa845dbf85f3b1aaa760cb3deb2e5f458
teruyamato0731 <teruyamato0731@gmail.com>
2024-10-02 02:44:04 +0000

    refactor: メンバ関数にする

--- examples/bin2.rs
+++ examples/bin2.rs
@@ -7,6 +7,30 @@ struct State {
     dtheta: f32,
 }
 
+impl State {
+    fn to_bytes(&self) -> [u8; 16] {
+        let mut buf = [0u8; 16];
+        buf[0..4].copy_from_slice(&self.x.to_le_bytes());
+        buf[4..8].copy_from_slice(&self.dx.to_le_bytes());
+        buf[8..12].copy_from_slice(&self.theta.to_le_bytes());
+        buf[12..16].copy_from_slice(&self.dtheta.to_le_bytes());
+        buf
+    }
+
+    fn from_bytes(buf: [u8; 16]) -> Self {
+        let x = f32::from_le_bytes(buf[0..4].try_into().unwrap());
+        let dx = f32::from_le_bytes(buf[4..8].try_into().unwrap());
+        let theta = f32::from_le_bytes(buf[8..12].try_into().unwrap());
+        let dtheta = f32::from_le_bytes(buf[12..16].try_into().unwrap());
+        State {
+            x,
+            dx,
+            theta,
+            dtheta,
+        }
+    }
+}
+
 fn main() {
     let s = State {
         x: 1.2,
@@ -15,23 +39,8 @@ fn main() {
         dtheta: 7.8,
     };
 
-    let mut buf = [0u8; 16];
-    buf[0..4].copy_from_slice(&s.x.to_le_bytes());
-    buf[4..8].copy_from_slice(&s.dx.to_le_bytes());
-    buf[8..12].copy_from_slice(&s.theta.to_le_bytes());
-    buf[12..16].copy_from_slice(&s.dtheta.to_le_bytes());
-
-    // buf[0..4] を f32 に変換
-    let x = f32::from_le_bytes(buf[0..4].try_into().unwrap());
-    let dx = f32::from_le_bytes(buf[4..8].try_into().unwrap());
-    let theta = f32::from_le_bytes(buf[8..12].try_into().unwrap());
-    let dtheta = f32::from_le_bytes(buf[12..16].try_into().unwrap());
-    let s2 = State {
-        x,
-        dx,
-        theta,
-        dtheta,
-    };
+    let buf = s.to_bytes();
+    let s2 = State::from_bytes(buf);
 
     println!("{:?}", s);
     println!("{:?}", buf);
