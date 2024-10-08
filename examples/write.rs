6635531fdd6f5cb71ecd658efb2ca8ba029b579b
teruyamato0731 <teruyamato0731@gmail.com>
2024-10-08 02:21:02 +0000

    feat: cobsで制御指令の書き込み

---
+++ examples/write.rs
@@ -0,0 +1,21 @@
+use std::time::Duration;
+use uart_rs_test::Control;
+use zerocopy::AsBytes;
+
+fn main() {
+    let mut port = serialport::new("/dev/ttyUSB0", 115_200)
+        .timeout(Duration::from_millis(10))
+        .open()
+        .expect("Failed to open port");
+
+    loop {
+        let c = Control { u: 1234 };
+        let buf = c.as_bytes();
+        let cobs: [u8; 4] = cobs_rs::stuff::<2, 4>(buf[0..2].try_into().unwrap(), 0);
+        println!("{:?}", cobs);
+        port.write_all(&cobs).expect("Write failed!");
+
+        // 500ms待機
+        std::thread::sleep(Duration::from_millis(1500));
+    }
+}
